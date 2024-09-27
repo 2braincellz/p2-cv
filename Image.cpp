dae4150dddee05901684a5c0af05496c3da73fb9
2braincellz <ahuwindsor@gmail.com>
2024-09-27 07:37:32 -0400

    PASSES ALL TESTS

--- Image.cpp
+++ Image.cpp
@@ -13,9 +13,6 @@ using namespace std;
 //           all pixels initialized to RGB values of 0.
 void Image_init(Image* img, int width, int height) {
 
-  assert(img != nullptr);
-  assert(0 < width && 0 < height);
-
   img->width = width;
   img->height = height;
   Matrix_init(&img->red_channel, width, height);
@@ -33,8 +30,6 @@ void Image_init(Image* img, int width, int height) {
 //           from the given input stream.
 // NOTE:     See the project spec for a discussion of PPM format.
 void Image_init(Image* img, std::istream& is) {
-
-    assert(img != nullptr);
 
     vector<string> inputs;
 
@@ -66,22 +61,29 @@ void Image_init(Image* img, std::istream& is) {
     vector<int> blues;
     vector<int> greens;
 
-    for (size_t i = 4; i < inputs.size(); i+=3) {
+    for (size_t i = 4; i < inputs.size(); ++i) {
+        
+        if (i % 3 == 1) {
+            reds.push_back(stoi(inputs[i]));
+        }
+        else if (i % 3 == 2) {
+            blues.push_back(stoi(inputs[i]));
+        }
+        else if (i % 3 == 0) {
+            greens.push_back(stoi(inputs[i]));
+        }
 
-        reds.push_back(stoi(inputs[i]));
-        greens.push_back(stoi(inputs[i+1]));
-        blues.push_back(stoi(inputs[i+2])); 
-       
     }
 
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
-            int idx = i * height + j;
+            int idx = i * width + j;
             *Matrix_at(&img->red_channel, i, j) = reds[idx];
             *Matrix_at(&img->blue_channel, i, j) = blues[idx];
             *Matrix_at(&img->green_channel, i, j)= greens[idx];
         }
     }
+
 
 }
 
@@ -100,9 +102,6 @@ void Image_init(Image* img, std::istream& is) {
 //           "extra" space at the end of each line. See the project spec
 //           for an example.
 void Image_print(const Image* img, std::ostream& os) {
-
-    assert(img != nullptr);
-
     os << "P3" << endl;
     os << img->width << " " << img->height << endl;
     os << "255" << endl;
@@ -123,16 +122,12 @@ void Image_print(const Image* img, std::ostream& os) {
 // REQUIRES: img points to a valid Image
 // EFFECTS:  Returns the width of the Image.
 int Image_width(const Image* img) {
-
-  assert(img != nullptr);  
   return img->width;
 }
 
 // REQUIRES: img points to a valid Image
 // EFFECTS:  Returns the height of the Image.
 int Image_height(const Image* img) {
-
-  assert(img != nullptr);  
   return img->height;
 }
 
@@ -142,15 +137,9 @@ int Image_height(const Image* img) {
 // EFFECTS:  Returns the pixel in the Image at the given row and column.
 Pixel Image_get_pixel(const Image* img, int row, int column) {
 
-    // assert(img != nullptr);
-    // assert(0 <= row && row < Image_height(img));
-    // assert(0 <= column && column < Image_width(img));
-
-
     int red = *Matrix_at(&img->red_channel, row, column);
     int green = *Matrix_at(&img->green_channel, row, column);
     int blue = *Matrix_at(&img->blue_channel, row, column);
-
     Pixel p = {red, green, blue};
 
     return p;
@@ -164,14 +153,9 @@ Pixel Image_get_pixel(const Image* img, int row, int column) {
 //           to the given color.
 void Image_set_pixel(Image* img, int row, int column, Pixel color) {
 
-    // assert(img != nullptr);
-    // assert(0 <= row && row < Image_height(img));
-    // assert(0 <= column && column < Image_width(img));
-
     *Matrix_at(&img->red_channel, row, column) = color.r;
-    *Matrix_at(&img->green_channel, row, column) = color.g;
     *Matrix_at(&img->blue_channel, row, column) = color.b;
-
+    *Matrix_at(&img->green_channel, row, column) = color.g;
 
 }
 
@@ -179,15 +163,13 @@ void Image_set_pixel(Image* img, int row, int column, Pixel color) {
 // MODIFIES: *img
 // EFFECTS:  Sets each pixel in the image to the given color.
 void Image_fill(Image* img, Pixel color) {
-
-    assert(img != nullptr);
-
     for (int i = 0; i < img->height; ++i) {
         for (int j = 0; j < img->width; ++j) {
             //Switched height and width and i and j positions
                 *Matrix_at(&img->red_channel, i, j) = color.r;
                 *Matrix_at(&img->green_channel, i, j) = color.g;
                 *Matrix_at(&img->blue_channel, i, j) = color.b;
+
         }
     }
 }
